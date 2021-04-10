76291370676d21c9b1bb6f164e962f67c8214f75
axmadjon <axiprogrammer@gmail.com>
2021-04-10 09:41:30 +0500

    commit_commit134

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:ussd_uz/constants/constant.dart';
 import 'package:ussd_uz/pages/home_screen/home_screen.dart';
-import 'package:ussd_uz/pages/internet_paketlar/internet_page.dart';
+import 'package:ussd_uz/pages/internet/internet_paketlar/internet_page.dart';
 import 'package:ussd_uz/pages/language_page/language_screen.dart';
 import 'package:ussd_uz/pages/main_screen/main_screen.dart';
 import 'package:ussd_uz/pages/splash_screen/splash_screen.dart';
 import 'package:ussd_uz/theme.dart';
+import 'package:path_provider/path_provider.dart' as path_pro;
 
 void main()async{
-  
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocDir=await path_pro.getApplicationDocumentsDirectory();
+  Hive.init(appDocDir.path);
   runApp(MyApp());
+  final box=await Hive.openBox("hive_db");
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme:basicTheme(context),
       routes: {
         MainScreen.id:(context)=>MainScreen.screen(),
-        InternetPage.id:(context)=>InternetPage.screen(),
+        InternetPage.id:(context)=>InternetPage.screen(firstPageColor),
       },
       home: MainScreen.screen(),
       debugShowCheckedModeBanner: false,
